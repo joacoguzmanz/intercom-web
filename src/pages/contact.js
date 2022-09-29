dc75d7516aac3ddc5a159e099742cb79c15851a9
JoaquinGuzman <49413000+joacoguzmanz@users.noreply.github.com>
2022-09-28 23:38:25 -0300

    second part finished

---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const ContactPage = () => {
+  return (
+    <Layout pageTitle='Contacto'>
+      <p>Página de contacto en vivo</p>
+    </Layout>
+  );
+};
+
+export const Head = () => <title>Página de contacto</title>;
+
+export default ContactPage;
