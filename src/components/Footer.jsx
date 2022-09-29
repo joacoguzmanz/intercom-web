2152ca89c07008649d4f0d0df0ec744725ab3e11
JoaquinGuzman <49413000+joacoguzmanz@users.noreply.github.com>
2022-09-29 11:00:33 -0300

    working on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
-// import { Link } from 'gatsby';
+import { Link } from 'gatsby';
+import { footerContainer, logo } from './styles/footer.module.css';
 
 const Footer = () => {
-  return <h1>Footer</h1>;
+  return (
+    <div className={footerContainer}>
+      <Link to='/' className={logo}>
+        InterCom.
+      </Link>
+
+      <div>
+        <h3>Contacto</h3>
+        <a href='mailto:arturogusman@hotmail.com'>arturogusman@hotmail</a>
+      </div>
+
+      <div>
+        <h3>Redes Sociales</h3>
+        <a href='https://www.instagram.com/portero.electrico/'>Instagram</a>
+      </div>
+
+      <p>&#169; 2022 InterCom. Porteros Eléctricos</p>
+    </div>
+  );
 };
 
 export default Footer;
