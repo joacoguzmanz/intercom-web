bbafabe2772b099970ec799e459fe17571cf34f0
JoaquinGuzman <49413000+joacoguzmanz@users.noreply.github.com>
2022-09-28 23:58:36 -0300

    image added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
 
-const PageIndex = () => {
+const IndexPage = () => {
   return (
     <Layout pageTitle='Home page'>
       <p>Probando la página de inicio</p>
+      <StaticImage
+        alt='Dogo looking at camera'
+        src='https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large'
+      />
     </Layout>
   );
 };
 
 export const Head = () => <title>Home page</title>;
 
-export default PageIndex;
+export default IndexPage;
