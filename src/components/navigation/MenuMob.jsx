e3a26737e83a0cd1b8f515d499bef527812c0797
Joaquin Guzman <joaquingusman77@gmail.com>
2024-06-11 11:34:27 +0200

    Update MenuMob.jsx

--- src/components/navigation/MenuMob.jsx
+++ src/components/navigation/MenuMob.jsx
@@ -5,7 +5,6 @@ const MenuMob = ({ menuOpen }) => {
         <div className={`${menuOpen ? 'block' : 'hidden'} md:hidden mt-2 bg-blue-700 w-11/12 mx-auto py-5 px-2.5 text-white text-center flex flex-col gap-2 rounded-lg`}>
             <Link href={'/empresa'} className='rounded-lg p-2.5'>Empresa</Link>
             <Link href={'/servicios'} className='rounded-lg p-2.5'>Servicios</Link>
-            <Link href={'/trabajos'} className='rounded-lg p-2.5'>Trabajos</Link>
             <Link href={'/contacto'} className='rounded-lg p-2.5'>Contacto</Link>
         </div>
     )
